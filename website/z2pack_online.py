81523c39ac7c9bc16657ffa68d754b774f8cc8c9
greschd <greschd@ethz.ch>
2014-09-16 13:35:34 +0200

    04.Commit	fixed the website design and expanded it a bit

--- website/z2pack_online.py
+++ website/z2pack_online.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr
+    Z2Pack online
     ~~~~~~
-
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-
-    :copyright: (c) 2010 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
+    
+    A calculator for the Z2 topological invariant in the tight-binding
+    model. 
 """
 
 from sqlite3 import dbapi2 as sqlite3
@@ -21,18 +18,32 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def calculate():
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/input', methods=['GET','POST'])
+def input():
     error = None
     if request.method == 'POST':
-        print( request.values["a1_x1"])
-        print( request.values["atompos_2_x"])
-        return render_template('z2pack_online.html')
+        check_input = False
+        #----------------check all input variables----------------------#
+        check_input = True #DEBUG
+        if(check_input):
+            return redirect(url_for('results'))
+        else:
+            return render_template('input.html')
     else:
         #~ print( request.values["a1_x1"])
-        return render_template('z2pack_online.html')
+        return render_template('input.html')
+        
+@app.route('/not_implemented')
+def not_implemented():
+    return render_template('not_implemented.html')
 
-
+@app.route('/results')
+def results():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run()

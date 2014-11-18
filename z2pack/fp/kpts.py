5dd478c02d9cb341ce70d0733ce79b54374f1a91
greschd <greschd@ethz.ch>
2014-11-18 10:17:53 +0100

    74.Commit	added kpts.py received from Gabriel Autes (Wannier90, Quantum Espresso)
    - reformatted to fit pep8

--- z2pack/fp/kpts.py
+++ z2pack/fp/kpts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-# Author:  Dominik Gresch <greschd@ethz.ch>
+# Authors:  Dominik Gresch <greschd@ethz.ch>, Gabriel Autes
 # Date:    27.09.2014 21:27:27 CEST
 # File:    kpts.py
 """
@@ -45,4 +45,41 @@ def abinit(start_point, last_point, end_point, N):
     for coord in last_point:
         string += str(coord).replace('e', 'd') + ' '
     string += '\n'
+    return string
+
+
+def qe(start_point, last_point, end_point, N):
+    """
+    For use with Quantum Espresso
+    """
+    for point in [start_point, last_point, end_point]:
+        if len(point) != 3:
+            raise ValueError('dimension of point != 3')
+
+    string = "\nK_POINTS crystal_b\n 2 \n"
+    for coord in start_point:
+        string += str(coord).replace('e', 'd') + ' '
+    string += str(N-1)+'\n'
+    for coord in last_point:
+        string += str(coord).replace('e', 'd') + ' '
+    string += str(1)+'\n'
+    return string
+
+
+def wannier90(start_point, last_point, end_point, N):
+    """
+    For use with Wannier90
+    """
+    for point in [start_point, last_point, end_point]:
+        if len(point) != 3:
+            raise ValueError('dimension of point != 3')
+
+    string = "mp_grid: " + str(int(N)) + " 1 1 \nbegin kpoints"
+    for i in range(N):
+        string += '\n'
+        point = start_point
+        for j, coord in enumerate(point):
+            coord += float(i)/float(N-1) * (last_point[j] - start_point[j])
+            string += str(coord).replace('e', 'd') + ' '
+    string += '\nend kpoints\n'
     return string

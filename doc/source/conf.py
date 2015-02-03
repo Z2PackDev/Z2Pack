b70c2f1afd555af8764fcd898559942977b22826
greschd <greschd@ethz.ch>
2015-02-03 16:16:20 +0100

    126.Commit	theme update pulled into v2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -106,16 +107,18 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'basicstrap'
+#~ html_theme = 'basicstrap'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-  'inner_theme': True,
-  'inner_theme_name': 'bootswatch-flatly',
-  'nav_fixed_top': False
-}
+#~ html_theme_options = {
+  #~ 'inner_theme': True,
+  #~ 'inner_theme_name': 'bootswatch-darkly',
+  #~ 'nav_fixed_top': False
+#~ }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #~ html_theme_path = ["."]

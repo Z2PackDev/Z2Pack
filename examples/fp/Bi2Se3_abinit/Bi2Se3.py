084001cb3cfd5aa99113fe25752a6ac664d3c34d
Dominik Gresch <greschd@ethz.ch>
2015-08-12 16:48:25 +0200

    322.Commit	working on resolving the Bi2Se3 example problem

--- examples/fp/Bi2Se3_abinit/Bi2Se3.py
+++ examples/fp/Bi2Se3_abinit/Bi2Se3.py
@@ -8,6 +8,7 @@
 import sys
 sys.path.append("../../../")
 import z2pack
+print(z2pack.__version__)
 
 import os
 
@@ -21,22 +22,13 @@ if __name__ == "__main__":
         os.makedirs('./results')
 
     # creating the z2pack.abinit object
-<<<<<<< HEAD
-    Bi2Se3 = z2pack.fp.System(['input/' + name for name in ["Bi2Se3_nscf.files", "Bi2Se3_nscf.in", "w90.win", "w.win"]],
-                            z2pack.fp.kpts.abinit,
-                            "Bi2Se3_nscf.in",
-                            "mpirun $ABINIT < Bi2Se3_nscf.files >& log",
-                            executable='/bin/bash'
-                            )
-=======
     Bi2Se3 = z2pack.fp.System(
         ['input/' + name for name in ["Bi2Se3_nscf.files", "Bi2Se3_nscf.in", "wannier90.win" ]],
         z2pack.fp.kpts.abinit,
         "Bi2Se3_nscf.in",
-        "mpirun -np 7 abinit < Bi2Se3_nscf.files >& log",
+        "abinit < Bi2Se3_nscf.files >& log",
         executable='/bin/bash'
     )
->>>>>>> 5a50e0d8de3f1a4e3f5204df92ad7db1704eeb71
 
     # surface at k1=0
     surface_0 = Bi2Se3.surface(lambda t: [0, t / 2, 0], [0, 0, 1], pickle_file = 'results/Bi2Se3_0.txt')

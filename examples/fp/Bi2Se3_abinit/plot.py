7eb26568cd6cda888e2d85a62d38a1209e96cdbf
Dominik Gresch <greschd@ethz.ch>
2015-08-14 09:24:33 +0200

    331.Commit	updated Bi2Se3 example

--- examples/fp/Bi2Se3_abinit/plot.py
+++ examples/fp/Bi2Se3_abinit/plot.py
@@ -21,31 +21,40 @@ if not os.path.exists('./results'):
 
 
 # creating the z2pack.abinit object
-Bi2Se3 = z2pack.fp.System(["Bi2Se3_nscf.files", "Bi2Se3_nscf.in", "wannier90.win" ],
-                        z2pack.fp.kpts.abinit,
-                        "Bi2Se3_nscf.in",
-                        "build",
-                        "mpirun -np 7 abinit < Bi2Se3_nscf.files >& log")
+system = z2pack.fp.System(
+    ["Bi2Se3_nscf.files", "Bi2Se3_nscf.in", "wannier90.win" ],
+    z2pack.fp.kpts.abinit,
+    "Bi2Se3_nscf.in",
+    "build",
+    "mpirun -np 7 abinit < Bi2Se3_nscf.files >& log"
+)
 
 
 # creating the z2pack.surface object
-surface_0 = Bi2Se3.surface(lambda t: [0, t / 2, 0], [0, 0, 1], pickle_file = './results/res_0.txt')
-surface_1 = Bi2Se3.surface(lambda t: [0.5, t / 2, 0], [0, 0, 1], pickle_file = './results/res_1.txt')
+surface_0 = system.surface(
+    lambda t: [0, t / 2, 0],
+    [0, 0, 1],
+    pickle_file='./results/res_0.txt'
+)
+surface_1 = system.surface(
+    lambda t: [0.5, t / 2, 0],
+    [0, 0, 1],
+    pickle_file='./results/res_1.txt'
+)
 
 
 surface_0.load()
 surface_1.load()
 fig, ax = plt.subplots(2, sharex=True, figsize = (9,5))
 fig.subplots_adjust(hspace=0.4)
-surface_0.plot(show = False, axis=ax[0], shift=0.5)
-ax[0].set_title(r'$k_1 = 0,$ $\Delta = {}$'.format(surface_0.invariant()))
-ax[0].set_ylabel(r'$x_3$')
-ax[0].set_xlabel('')
-surface_1.plot(show = False, axis=ax[1], shift=0.5)
-ax[1].set_title(r'$k_1 = 0.5,$ $\Delta = {}$'.format(surface_1.invariant()))
-ax[1].set_ylabel(r'$x_3$')
+surface_0.wcc_plot(show = False, axis=ax[0], shift=0.5)
+ax[0].set_title(r'$k_1 = 0,$ $\Delta = {}$'.format(surface_0.z2()))
+ax[0].set_ylabel(r'$\bar{x}_3$', rotation='horizontal')
+surface_1.wcc_plot(show = False, axis=ax[1], shift=0.5)
+ax[1].set_title(r'$k_1 = 0.5,$ $\Delta = {}$'.format(surface_1.z2()))
+ax[1].set_ylabel(r'$\bar{x}_3$', rotation='horizontal')
 ax[1].set_xlabel(r'$k_2$')
-plt.savefig('./results/Bi2Se3.pdf', bbox_inches = 'tight')
+plt.savefig('./results/plot.pdf', bbox_inches='tight')
 
 
 
--- examples/fp/Bi2Se3_abinit/run.py
+++ examples/fp/Bi2Se3_abinit/run.py
@@ -3,12 +3,11 @@
 #
 # Author:  Dominik Gresch <greschd@ethz.ch>
 # Date:    14.08.2014 12:18:25 CEST
-# File:    BiSe.py
+# File:    run.py
 
 import sys
 sys.path.append("../../../")
 import z2pack
-print(z2pack.__version__)
 
 import os
 
@@ -21,9 +20,11 @@ if __name__ == "__main__":
     if not os.path.exists('./results'):
         os.makedirs('./results')
 
-    # creating the z2pack.abinit object
-    Bi2Se3 = z2pack.fp.System(
-        ['input/' + name for name in ["Bi2Se3_nscf.files", "Bi2Se3_nscf.in", "wannier90.win" ]],
+    # creating the z2pack.fp object
+    system = z2pack.fp.System(
+        ['input/' + name for name in [
+            "Bi2Se3_nscf.files", "Bi2Se3_nscf.in", "wannier90.win"
+        ]],
         z2pack.fp.kpts.abinit,
         "Bi2Se3_nscf.in",
         "abinit < Bi2Se3_nscf.files >& log",
@@ -31,11 +32,19 @@ if __name__ == "__main__":
     )
 
     # surface at k1=0
-    surface_0 = Bi2Se3.surface(lambda t: [0, t / 2, 0], [0, 0, 1], pickle_file = 'results/Bi2Se3_0.txt')
+    surface_0 = system.surface(
+        lambda t: [0, t / 2, 0],
+        [0, 0, 1],
+        pickle_file='results/res_0.txt'
+    )
     surface_0.load(quiet=True)
     surface_0.wcc_calc()
 
     # surface at k1=Pi
-    surface_1 = Bi2Se3.surface(lambda t: [0.5, t / 2, 0], [0, 0, 1], pickle_file = 'results/Bi2Se3_1.txt')
+    surface_1 = system.surface(
+        lambda t: [0.5, t / 2, 0],
+        [0, 0, 1],
+        pickle_file='results/res_1.txt'
+    )
     surface_1.load(quiet=True)
     surface_1.wcc_calc()

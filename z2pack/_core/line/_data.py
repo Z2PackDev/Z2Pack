27e5b4add2f37d0e7953fe9c13140f7b02e0535c
Dominik Gresch <greschd@ethz.ch>
2016-02-09 10:07:44 +0100

    added deletion of calculated attributes on setting overlaps to line data

--- z2pack/_core/line/_data.py
+++ z2pack/_core/line/_data.py
@@ -11,34 +11,53 @@ class LineData(object):
     def __init__(self, overlaps):
         self.overlaps = overlaps
 
+    # Helpers for the automatically calculated attributes / properties
+    _calculated_attrs = []
+
+    def _property_helper(name_list, name):
+        """Checks whether an attribute of the given name exists. If it does not, the decorated function is executed, which should produce the attribute. Finally, the attribute is returned."""
+        name_list.append(name)
+        def dec(f):
+            def inner(self):
+                if not hasattr(self, name):
+                    f(self)
+                return getattr(self, name)
+            return inner
+        return dec
+
+    def __setattr__(self, key, value):
+        if key == 'overlaps':
+            for name in self.__class__._calculated_attributes:
+                try:
+                    delattr(self, name)
+                except AttributeError:
+                    pass
+        super(Foo, self).__setattr__(key, value)
+
     @property
+    @_property_helper('_calculated_attrs', '_lambda_')
     def lambda_(self):
         lambda_ = np.eye(len(self.overlaps[0]))
-        if not hasattr(self, '_lambda_'):
-            for M in self.overlaps:
-                [V, E, W] = la.svd(M)
-                lambda_ = np.dot(np.dot(V, W).conjugate().transpose(), lambda_)
+        for M in self.overlaps:
+            [V, E, W] = la.svd(M)
+            lambda_ = np.dot(np.dot(V, W).conjugate().transpose(), lambda_)
         self._lambda_ = lambda_
-        return self._lambda_
 
     @property
+    @_property_helper('_calculated_attrs', '_wcc')
     def wcc(self):
-        if not hasattr(self, '_wcc'):
-            [eigs, _] = la.eig(self.lambda_)
-            self._wcc = sorted([(1j * np.log(z) / (2 * np.pi)).real % 1 for z in eigs])
-        return self._wcc
+        eigs, _ = la.eig(self.lambda_)
+        self._wcc = sorted([(1j * np.log(z) / (2 * np.pi)).real % 1 for z in eigs])
 
     @property
+    @_property_helper('_calculated_attrs', '_gap_pos')
     def gap_pos(self):
-        if not hasattr(self, '_gap_pos'):
-            self._calculate_gap()
-        return self._gap_pos
+        self._calculate_gap()
 
     @property
+    @_property_helper('_calculated_attrs', '_gap_size')
     def gap_size(self):
-        if not hasattr(self, '_gap_size'):
-            self._calculate_gap()
-        return self._gap_size
+        self._calculate_gap()
 
     def _calculate_gap(self):
         gap_size = 0

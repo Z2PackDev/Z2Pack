60c370edf18a7cdf49bc3c293ab3c657ac2bb5bd
Dominik Gresch <greschd@ethz.ch>
2016-02-09 12:03:24 +0100

    modified ABC for convergence objects to include any kind of control object

--- z2pack/_core/_convergence_abc.py
+++ z2pack/_core/_convergence_abc.py
@@ -9,22 +9,41 @@ import abc
 import six
 
 @six.add_metaclass(abc.ABCMeta)
-class ConvergenceTester(object):
-
+class AbstractControl(object):
+    """ABC for all control objects."""
     @abc.abstractmethod
-    def update(self, data):
+    def __init__(self, state=None, *args, **kwargs):
         pass
 
-    @abc.abstractproperty
-    def converged(self):
-        pass
-        
     @abc.abstractproperty
     def state(self):
         pass
 
-class SurfaceConvergenceTester(ConvergenceTester):
+class DataControl(AbstractControl):
+    """ABC for control objects which can be updated with data."""
+    @abc.abstractmethod
+    def update(self, data):
+        pass
+
+class IterationControl(AbstractControl):
+    """ABC for iteration control objects. Enforces the existence of ..."""
+    @abc.abstractmethod
+    def next(self):
+        pass
+
+class ConvergenceTester(DataControl):
+    """ABC for convergence tester objects. Enforces the existence of an update method, and the converged and state properties"""
+    @abc.abstractproperty
+    def converged(self):
+        pass
+
+# The only purpose of these subclasses is to distinguish between
+# ConvergenceTesters which take a SurfaceData object and those which take
+# a LineData object.
+class SurfaceControl(AbstractControl):
+    """Specializes AbstractControl for Surface objects""" 
     pass
 
-class LineConvergenceTester(ConvergenceTester):
+class LineControl(AbstractControl):
+    """Specializes AbstractControl for Line objects""" 
     pass

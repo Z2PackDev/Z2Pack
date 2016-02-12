9421674d0ed1d7dacc69e2cddd12b798f796d22f
Dominik Gresch <greschd@ethz.ch>
2016-02-13 00:38:26 +0100

    added rough outline for run of line

--- z2pack/_core/line/_run.py
+++ z2pack/_core/line/_run.py
@@ -5,5 +5,26 @@
 # Date:    12.02.2016 16:04:45 CET
 # File:    _run.py
 
+def _run_line_impl():
+    """
+    Wrapper for:
+        * getting / disecting old result
+        * setting up Controls
+            - from old result
+            - from input parameters
+        * setting up printing status
+        * setting up file backend
+    """
 
-def run_line():
+def _run_line_impl(*controls, save_file=None):
+    """
+    Input parameters:
+        * Controls
+        * file backend?
+    """
+    stateful_ctrl = [ctrl for ctrl in controls if isinstance(ctrl, StatefulControl)]
+    iteration_ctrl = ...
+    data_ctrl = ...
+    conv_ctrl = ...
+    ...
+    # main loop

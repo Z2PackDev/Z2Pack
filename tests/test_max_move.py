c9b97575ce0917f72e5c20dc649ab2cc36e9b7ce
Dominik Gresch <greschd@ethz.ch>
2016-02-19 15:59:01 -0700

    added another test for max_move

--- tests/test_max_move.py
+++ tests/test_max_move.py
@@ -21,6 +21,8 @@ def N(request):
 def l1(N):
     return []
 
+epsilon = 1e-14
+
 def test_zero(N):
     l1 = [random.random() for _ in range(N)]
     assert max_move(l1, l1) == 0
@@ -33,7 +35,7 @@ def test_single_move(N):
     idx = random.randint(0, N - 1)
     l2[idx] += move
     l2[idx] %= 1
-    assert(max_move(l1, l2) <= abs(move) + 1e-14)
+    assert(max_move(l1, l2) <= abs(move) + epsilon)
 
 def test_single_move_equal_spacing(N):
     l1 = list(np.linspace(0, 1, N, endpoint = False))
@@ -42,4 +44,15 @@ def test_single_move_equal_spacing(N):
     idx = random.randint(0, N - 1)
     l2[idx] += move
     l2[idx] %= 1
-    assert(abs(move) - 1e-14 <= max_move(l1, l2) <= abs(move) + 1e-14)
+    assert(abs(move) - epsilon <= max_move(l1, l2) <= abs(move) + epsilon)
+
+def test_move_all(N):
+    l1 = [random.random() for _ in range(N)]
+    l2 = copy.deepcopy(l1)
+    real_max_move = 0
+    for idx in range(N):
+        move = random.uniform(-1, 1) / N
+        real_max_move = max(abs(move), real_max_move)
+        l2[idx] += move
+        l2[idx] %= 1
+    assert(max_move(l1, l2) <= abs(real_max_move) + epsilon)

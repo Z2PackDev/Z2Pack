69ff5e521bc735dfb87d703c5b24c5aaf7709d87
Dominik Gresch <greschd@ethz.ch>
2016-03-11 14:24:55 +0100

    removed unnecessary (duplicate) Chern tests

--- tests/test_chern_invariant.py
+++ tests/test_chern_invariant.py
@@ -33,27 +33,21 @@ def x(request):
     return request.param
 
 def test_trivial(N, M, patch_surface_data):
+    """Test straight WCC lines"""
     wcc = [np.linspace(0, 1, M) for j in range(N)]
     data = SurfaceData(wcc)
     assert z2pack.surface.invariant.chern(data) == 0
 
-def test_linear(L, x, patch_surface_data):
-    wcc = np.array([np.linspace(x,  1 + x, L)]).T
-    data = SurfaceData(wcc)
-    assert np.isclose(z2pack.surface.invariant.chern(data), 1)
-
-def test_linear_2(L, x, offset, patch_surface_data):
+def test_linear(L, x, offset, patch_surface_data):
+    """Test a linear offset"""
     wcc = np.array([np.linspace(x,  offset + x, L)]).T
     data = SurfaceData(wcc)
     assert (abs(offset) / (L - 1) >= 0.5) or np.isclose(z2pack.surface.invariant.chern(data), offset)
 
-def test_linear_3(M, L, x, offset, patch_surface_data):
+def test_linear_2(M, L, x, offset, patch_surface_data):
+    """Test a linear offset, with some additional lines in between"""
     wcc = [np.linspace(x,  offset + x, L)]
     wcc += [[random.random()] * L] * M
     wcc = np.array(wcc).T
-    #~ wcc = list(zip(np.linspace(0, 0.6, L), np.linspace(1, 0.6, L)))
-    #~ rand = list(sorted([random.random() for _ in range(M)]))
-    #~ wcc += [rand] * L
-    #~ wcc = np.array(wcc)
     data = SurfaceData(wcc)
     assert (abs(offset) / (L - 1) >= 0.5) or np.isclose(z2pack.surface.invariant.chern(data), offset)

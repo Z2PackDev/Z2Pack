f320f3790a0396e6229e0d3bb527d75d532c6076
Dominik Gresch <greschd@ethz.ch>
2016-03-11 14:01:46 +0100

    added test for z2

--- tests/test_z2_invariant.py
+++ tests/test_z2_invariant.py
@@ -20,7 +20,29 @@ def N(request):
 def M(request):
     return request.param
 
+@pytest.fixture(params=range(4, 20))
+def L(request):
+    return request.param
+
+@pytest.fixture(params=np.linspace(0.05, 0.95, 10))
+def x(request):
+    return request.param
+
 def test_trivial(N, M, patch_surface_data):
     wcc = [np.linspace(0, 1, M) for j in range(N)]
     data = SurfaceData(wcc)
     assert z2pack.surface.invariant.z2(data) == 0
+
+def test_linear(L, x, patch_surface_data):
+    wcc = np.array([np.linspace(0, x, L), np.linspace(1, x, L)]).T
+    data = SurfaceData(wcc)
+    assert z2pack.surface.invariant.z2(data) == 1
+    
+def test_linear_2(M, L, patch_surface_data):
+    M *= 2
+    wcc = list(zip(np.linspace(0, 0.6, L), np.linspace(1, 0.6, L)))
+    rand = list(sorted([random.random() for _ in range(M)] * 2))
+    wcc += [rand] * L
+    wcc = np.array(wcc)
+    data = SurfaceData(wcc)
+    assert z2pack.surface.invariant.z2(data) == 1

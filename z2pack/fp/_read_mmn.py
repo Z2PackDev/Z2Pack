3d76d69d3497d2e7b574f68ed29c48be85701b54
Dominik Gresch <greschd@ethz.ch>
2016-03-18 02:18:03 +0100

    improved (?) mmn parsing

--- z2pack/fp/_read_mmn.py
+++ z2pack/fp/_read_mmn.py
@@ -6,6 +6,7 @@
 # File:    _read_mmn.py
 
 import re
+import numpy as np
 
 def get_m(mmn_file):
     """
@@ -19,49 +20,43 @@ def get_m(mmn_file):
         with open(mmn_file, "r") as f:
             f.readline()
 
+            re_int = re.compile(r'[\d]+')
+
             # read the first line
-            line = re.findall(r'[\d]+', f.readline())
-            num_bands, num_kpts, _ = [int(l) for l in line]
+            num_bands, num_kpts, _ = (int(i) for i in re.findall(
+                re_int,
+                f.readline()
+            ))
 
             # read the rest of the file
-            data = f.read()
+            lines = (line for line in f if line)
+
+            step = num_bands * num_bands + 1
+            def grouper(iterable, n):
+                """Collect data into fixed-length chunks or blocks"""
+                args = [iter(iterable)] * n
+                return zip(*args)
+            blocks = grouper(lines, step)
+            M = []
+            re_float = re.compile(r'[0-9.\-E]+')
+            for block in blocks:
+                block = iter(block)
+                idx = [int(el) for el in re.findall(re_int, next(block))]
+                if idx[0] % num_kpts - idx[1] != -1:
+                    continue
+
+                def to_complex(blockline):
+                    k1, k2 = re.findall(re_float, blockline)
+                    return float(k1) + 1j * float(k2)
+
+                M.append(np.array([
+                    [to_complex(next(block)) for _ in range(num_bands)]
+                    for _ in range(num_bands)
+                ]).T)
+
     except IOError as err:
         msg = str(err)
         msg += '. Check that the path of the .mmn file is correct (mmn_path input variable). If that is the case, an error occured during the call to the first-principles code and Wannier90. Check the corresponding log/error files.'
         raise IOError(msg)
-
-    data = [entry for entry in data.split("\n") if entry]
-    blocks = []
-    step = num_bands * num_bands + 1
-    for i in range(0, len(data), step):
-        blocks.append([entry for entry in data[i:i + step] if entry])
-
-    # extract k and k + b for each M
-    idx_list = []
-    for i in range(len(blocks)):
-        idx_list.append(
-            [int(el) for el in re.findall(r'[\d]+', blocks[i][0])[:2]]
-        )
-
-    # extract M
-    M = []
-    for i in range(len(blocks)):
-        # check if element has to be in the string
-        if idx_list[i][0] % num_kpts - idx_list[i][1] != -1:
-            continue
-        # end check
-        temp = []
-        for j in range(1, len(blocks[i])):
-            temp2 = [
-                float(k) for k in
-                re.findall(r'[0-9.\-E]+', blocks[i][j])
-            ]
-            temp.append(temp2[0] + 1j * temp2[1])
-
-        temp2 = []
-        for j in range(0, len(temp), num_bands):
-            temp2.append(temp[j:j + num_bands])
-
-        M.append([list(x) for x in zip(*temp2)])
 
     return M

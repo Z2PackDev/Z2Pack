479611b15ac32705e64820506dde65e39d8ad3e7
Dominik Gresch <greschd@gmx.ch>
2016-07-21 14:36:25 +0200

    added logging test for line output

--- tests/test_log.py
+++ tests/test_log.py
@@ -7,39 +7,45 @@
 
 import sys
 from io import StringIO
+from contextlib import contextmanager
 
 import z2pack
 import logging
 
 
-from em_systems import simple_system, simple_surface
+from em_systems import simple_system, simple_surface, simple_line
+
+IGNORE_LINES = ['Calculation finished', 'starting at', 'Z2Pack version', ' at 0x']
 
 def compare_lines(x, y):
     for xline, yline in zip(x.splitlines(), y.splitlines()):
-        assert x == y
+        if any((part in xline) and (part in yline) for part in IGNORE_LINES):
+            continue
+        assert xline == yline
     return True
 
-def test_report(capsys, compare_data, simple_system, simple_surface):
+@contextmanager
+def CaptureLoggingOutput(compare_data):
     out = StringIO()
     handler = logging.StreamHandler(stream=out)
     handler.setFormatter(z2pack._logging_format.DefaultFormatter())
     logger = logging.getLogger('z2pack')
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
-    result = z2pack.surface.run(system=simple_system, surface=simple_surface)
+    yield
     handler.flush()
     handler.close()
     logger.removeHandler(handler)
     out.seek(0)
-    lines = out.read().splitlines()
-    lines_notime = (
-        l for l in lines if not (
-            'Calculation finished' in l or
-            'starting at' in l or
-            'Z2Pack version' in l or
-            ' at 0x' in l
-        )
-    )
-    res = '\n'.join(lines_notime)
+    res = out.read()
     compare_data(compare_lines, res)
+        
+
+def test_surface_report(compare_data, simple_system, simple_surface):
+    with CaptureLoggingOutput(compare_data):
+        result = z2pack.surface.run(system=simple_system, surface=simple_surface)
+
+def test_line_report(compare_data, simple_system, simple_line):
+    with CaptureLoggingOutput(compare_data):
+        result = z2pack.line.run(system=simple_system, line=simple_line)
     

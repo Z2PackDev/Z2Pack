3d098f0bd2bae8854d657739ec6bc228749b45ca
Dominik Gresch <greschd@gmx.ch>
2016-08-11 00:15:29 +0200

    working on Bi2Se3 example (ABINIT)

--- examples/fp/Bi2Se3_abinit/run.py
+++ examples/fp/Bi2Se3_abinit/run.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Author:  Dominik Gresch <greschd@ethz.ch>
-# Date:    14.08.2014 12:18:25 CEST
-# File:    BiSe.py
-
-"""
-Bismuth Selenide example
-"""
-
-import sys
-sys.path.append("../../../")
-import z2pack
+# Author:  Dominik Gresch <greschd@gmx.ch>
+# Date:    11.08.2016 00:03:52 CEST
+# File:    run.py
 
 import os
 
-if __name__ == "__main__":
+import z2pack
+import matplotlib.pyplot as plt
 
-    if not os.path.exists('./results'):
-        os.makedirs('./results')
+# Creating the System. Note that the SCF charge file does not need to be
+# copied, but instead can be referenced in the .files file.
+# The k-points input is appended to the .in file
+# The command (mpirun ...) will have to be replaced to match your system.
+SYSTEM_NAME = 'Bi2Se3'
+system = z2pack.fp.System(
+    input_files=[os.path.join('input', filename) for filename in [
+        SYSTEM_NAME + '_nscf.files',
+        SYSTEM_NAME + '_nscf.in',
+        'wannier90.win'
+    ]],
+    kpt_fct=z2pack.fp.kpoint.abinit,
+    kpt_path=SYSTEM_NAME + '_nscf.in',
+    command='mpirun -np 4 ~/software/abinit-7.10.5/src/98_main/abinit < {}_nscf.files >& log'.format(SYSTEM_NAME),
+    executable='/bin/bash'
+)
 
-    # creating the z2pack.abinit object
-    system = z2pack.fp.System(
-        ['input/' + name for name in [
-            'Bi2Se3_nscf.files', 'Bi2Se3_nscf.in', 'wannier90.win'
-        ]],
-        z2pack.fp.kpoint.abinit,
-        'Bi2Se3_nscf.in',
-        'abinit < Bi2Se3_nscf.files >& log',
-        executable='/bin/bash'
-    )
+# Surface calculation for surfaces at kx=0 and kx=0.5
+# Standard settings are used
+res_0 = z2pack.surface.run(
+    system=system, 
+    surface=lambda s, t: [0, s / 2, t],
+    save_file='res_0.msgpack'
+)
+res_1 = z2pack.surface.run(
+    system=system, 
+    surface=lambda s, t: [0.5, s / 2, t],
+    save_file='res_1.msgpack'
+)
 
-    # surface at k1=0
-    surface_0 = system.surface(
-        lambda t: [0, t / 2, 0],
-        [0, 0, 1],
-        pickle_file='results/Bi2Se3_0.txt'
-    )
-    surface_0.load(quiet=True)
-    surface_0.wcc_calc()
+# Combining the two plots
+fig, ax = plt.subplots(1, 2, sharey=True, figsize=(9, 5))
+z2pack.plot.wcc(res_0, axis=ax[0])
+z2pack.plot.wcc(res_1, axis=ax[1])
+plt.savefig('plot.pdf', bbox_inches='tight')
 
-    # surface at k1=Pi
-    surface_1 = system.surface(
-        lambda t: [0.5, t / 2, 0],
-        [0, 0, 1],
-        pickle_file='results/Bi2Se3_1.txt'
-    )
-    surface_1.load(quiet=True)
-    surface_1.wcc_calc()
+print('Z2 topological invariant at kx = 0: {0}'.format(z2pack.invariant.z2(res_0)))
+print('Z2 topological invariant at kx = 0.5: {0}'.format(z2pack.invariant.z2(res_1)))

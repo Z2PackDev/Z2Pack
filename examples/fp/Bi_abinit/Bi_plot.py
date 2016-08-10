a8a37f807e02f0ef9fff1eed7cb1d88dca600479
Dominik Gresch <greschd@gmx.ch>
2016-08-10 23:14:27 +0200

    updated plotting script

--- examples/fp/Bi_abinit/Bi_plot.py
+++ examples/fp/Bi_abinit/Bi_plot.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 
 import z2pack
 
-result_0 = z2pack.load_result('results/Bi_0.json')
-result_1 = z2pack.load_result('results/Bi_1.json')
+result_0 = z2pack.io.load('results/Bi_0.msgpack')
+result_1 = z2pack.io.load('results/Bi_1.msgpack')
 
 # plotting
 fig, ax = plt.subplots(1, 2)
@@ -25,17 +25,17 @@ for axis in ax:
     axis.set_xticklabels([r'$0$', r'$\pi$'])
 ax[0].set_ylabel(r'$\bar{x}$', rotation='horizontal', fontsize=fs)
 ax[0].set_title(
-    r'$k_x=0, \Delta={}$'.format(z2pack.surface.invariant.z2(result_0)),
+    r'$k_x=0, \Delta={}$'.format(z2pack.invariant.z2(result_0)),
     fontsize=fs
 )
 ax[1].set_title(
-    r'$k_x=0, \Delta={}$'.format(z2pack.surface.invariant.z2(result_1)),
+    r'$k_x=0, \Delta={}$'.format(z2pack.invariant.z2(result_1)),
     fontsize=fs
 )
 
 # plotting the WCC evolution
-z2pack.surface.plot.wcc(result_0, axis=ax[0])
-z2pack.surface.plot.wcc(result_1, axis=ax[1])
+z2pack.plot.wcc(result_0, axis=ax[0])
+z2pack.plot.wcc(result_1, axis=ax[1])
 
 if not os.path.isdir('plots'):
     os.mkdir('plots')

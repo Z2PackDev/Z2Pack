c33fc7ccf527bc9b03d6a74c0ce7323758577698
Dominik Gresch <greschd@gmx.ch>
2016-09-15 10:46:58 +0200

    added w90 tests to kpoint

--- tests/test_fp_kpoint.py
+++ tests/test_fp_kpoint.py
@@ -10,8 +10,8 @@ import numpy as np
 
 import z2pack
 
-@pytest.fixture(params=np.linspace(0, 1, 11))
-def kpt(request, line):
+@pytest.fixture
+def kpt(line):
     kpt = [np.array(line(tval)) for tval in np.linspace(0, 1, 11)]
     return kpt
 
@@ -37,28 +37,40 @@ straight_any_direction = [
     lambda t: [0, 1 - t, 0]
 ]
 
+straight_unequal_spacing = [
+    lambda t: [0, 0, t**2]
+]
+
+straight_multiple_bz = [
+    lambda t: [0, 0, 2 * t]
+]
+
 non_straight = [
-    lambda t: [0, 0, t**2],
-    lambda t: [0, 0, 2 * t],
     lambda t: [0, np.cos(2 * np.pi * t), np.sin(2 * np.pi * t)],
     lambda t: [0, 0.2 * np.cos(2 * np.pi * t), 0.3 * np.sin(2 * np.pi * t)],
 ]
+
+all_valid = straight_simple + straight_any_direction + straight_unequal_spacing + straight_multiple_bz + non_straight 
 
 invalid = [
     lambda t: [0, 0, 0.9 * t]
 ]
 
-ALL_LINES = straight_simple + straight_any_direction + non_straight + invalid
+ALL_LINES = all_valid + invalid
 
 VALID_LINES = {
-    z2pack.fp.kpoint.vasp.__name__: {'fct': z2pack.fp.kpoint.vasp, 'valid': straight_simple, 'invalid': straight_any_direction + non_straight + invalid},
-    z2pack.fp.kpoint.qe.__name__: {'fct': z2pack.fp.kpoint.qe, 'valid': straight_simple + straight_any_direction + non_straight, 'invalid': invalid}
+    z2pack.fp.kpoint.vasp.__name__: {'fct': z2pack.fp.kpoint.vasp, 'valid': straight_simple, 'invalid': straight_any_direction + straight_unequal_spacing + straight_multiple_bz + non_straight + invalid},
+    z2pack.fp.kpoint.qe.__name__: {'fct': z2pack.fp.kpoint.qe, 'valid': all_valid, 'invalid': invalid},
+    z2pack.fp.kpoint.abinit.__name__: {'fct': z2pack.fp.kpoint.abinit, 'valid': straight_simple + straight_any_direction + straight_multiple_bz, 'invalid': straight_unequal_spacing + non_straight + invalid},
+    z2pack.fp.kpoint.wannier90.__name__: {'fct': z2pack.fp.kpoint.wannier90, 'valid': all_valid, 'invalid': invalid},
+    z2pack.fp.kpoint.wannier90_nnkpts.__name__: {'fct': z2pack.fp.kpoint.wannier90_nnkpts, 'valid': all_valid, 'invalid': invalid},
+    z2pack.fp.kpoint.wannier90_full.__name__: {'fct': z2pack.fp.kpoint.wannier90_full, 'valid': all_valid, 'invalid': invalid}
 }
 
 
 @pytest.mark.parametrize('fct', sorted(VALID_LINES.keys()))
 @pytest.mark.parametrize('line', ALL_LINES)
-def test_valid_lines(kpt, fct, line, compare_equal):
+def test_lines(kpt, fct, line, compare_equal):
     if line in VALID_LINES[fct]['valid']:
         compare_equal(VALID_LINES[fct]['fct'](kpt))
     elif line in VALID_LINES[fct]['invalid']:

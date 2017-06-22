13a8e2e53fbb772bdb17b3d3002bb79673f2b46c
Dominik Gresch <greschd@gmx.ch>
2017-06-22 09:50:48 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
         'Development Status :: 5 - Production/Stable'
--- z2pack/_version.py
+++ z2pack/_version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '2.1.0a0'
+__version__ = '2.1.0'

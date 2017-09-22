26a14775a7e1e21c20235ea6613f50fb716c4bed
Dominik Gresch <greschd@gmx.ch>
2017-09-23 01:23:14 +0200

    Fix linter issues in line run and log tests

--- tests/test_line_run.py
+++ tests/test_line_run.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Tests for line calculations."""
+# pylint: disable=unused-wildcard-import,redefined-outer-name
 
 import os
 import json
-import inspect
 import tempfile
 
+import z2pack
 import pytest
 import numpy as np
-
-import z2pack
 
 from hm_systems import *
 
 
 def normalize_convergence_report(report):
-    # np.bool_ cannot be put into json
+    """
+    Converts values in convergence report from np.bool_ to bool.
+    """
     return {key: bool(val) for key, val in report.items()}
 
 
 def test_trivial_run(simple_system, simple_line, compare_equal):
+    """Test simple line run."""
     result = z2pack.line.run(system=simple_system, line=simple_line)
     assert result.wcc == [0, 0]
     assert result.gap_pos == 0.5
@@ -30,11 +31,13 @@ def test_trivial_run(simple_system, simple_line, compare_equal):
 
 
 def test_weyl(weyl_system, weyl_line, compare_data):
+    """Test line run for Weyl system."""
     result = z2pack.line.run(system=weyl_system, line=weyl_line)
     compare_data(lambda r1, r2: all(np.isclose(r1, r1).flatten()), result.wcc)
 
 
 def test_no_pos_tol(simple_system, simple_line, compare_equal):
+    """Test simple line run without pos_tol check."""
     result = z2pack.line.run(
         system=simple_system, line=simple_line, pos_tol=None
     )
@@ -43,11 +46,12 @@ def test_no_pos_tol(simple_system, simple_line, compare_equal):
     assert result.gap_size == 1
     assert result.ctrl_states['StepCounter'] == 8
     with pytest.raises(KeyError):
-        result.ctrl_states['PosCheck']
+        result.ctrl_states['PosCheck']  # pylint: disable=pointless-statement
     compare_equal(normalize_convergence_report(result.convergence_report))
 
 
 def test_pos_tol(weyl_system, weyl_line, pos_tol, compare_equal):
+    """Test simple line run with different pos_tol values."""
     result = z2pack.line.run(
         system=weyl_system, line=weyl_line, pos_tol=pos_tol
     )
@@ -58,6 +62,7 @@ def test_pos_tol(weyl_system, weyl_line, pos_tol, compare_equal):
 
 
 def test_iterator(simple_system, simple_line, compare_equal):
+    """Test simple line run with special iterator value."""
     result = z2pack.line.run(
         system=simple_system, line=simple_line, iterator=[5, 7, 9]
     )
@@ -66,6 +71,7 @@ def test_iterator(simple_system, simple_line, compare_equal):
 
 
 def test_iterator_2(weyl_system, compare_equal):
+    """Test simple line run with special iterator value."""
     result = z2pack.line.run(
         system=weyl_system,
         line=weyl_line(0.1),
@@ -77,6 +83,9 @@ def test_iterator_2(weyl_system, compare_equal):
 
 
 def test_iterator_3(simple_system, simple_line, compare_equal):
+    """
+    Test simple line run with special iterator value and no pos_tol check.
+    """
     result = z2pack.line.run(
         system=simple_system,
         line=simple_line,
@@ -88,6 +97,9 @@ def test_iterator_3(simple_system, simple_line, compare_equal):
 
 
 def assert_res_equal(result1, result2):
+    """
+    Check that two line results are equal.
+    """
     assert result1.wcc == result2.wcc
     assert result1.gap_pos == result2.gap_pos
     assert result1.gap_size == result2.gap_size
@@ -97,31 +109,36 @@ def assert_res_equal(result1, result2):
     assert result1.convergence_report == result2.convergence_report
 
 
-# saving tests
 def test_simple_save(simple_system, simple_line):
-    # Context manager doesn't work because the file is moved in the atomic save
-    fp = tempfile.NamedTemporaryFile(delete=False)
+    """
+    Test saving during a line run.
+    """
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
     result = z2pack.line.run(
-        system=simple_system, line=simple_line, save_file=fp.name
+        system=simple_system, line=simple_line, save_file=temp_file.name
     )
-    result2 = z2pack.io.load(fp.name, serializer=json)
-    os.remove(fp.name)
+    result2 = z2pack.io.load(temp_file.name, serializer=json)
+    os.remove(temp_file.name)
     assert_res_equal(result, result2)
 
 
 def test_weyl_save(weyl_system, weyl_line):
-    # Context manager doesn't work because the file is moved in the atomic save
-    fp = tempfile.NamedTemporaryFile(delete=False)
+    """
+    Test saving during a Weyl system line run.
+    """
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
     result = z2pack.line.run(
-        system=weyl_system, line=weyl_line, save_file=fp.name
+        system=weyl_system, line=weyl_line, save_file=temp_file.name
     )
-    result2 = z2pack.io.load(fp.name, serializer=json)
-    os.remove(fp.name)
+    result2 = z2pack.io.load(temp_file.name, serializer=json)
+    os.remove(temp_file.name)
     assert_res_equal(result, result2)
 
 
-# test restart
 def test_restart(simple_system, simple_line):
+    """
+    Test restarting from an init_result.
+    """
     result = z2pack.line.run(system=simple_system, line=simple_line)
     result2 = z2pack.line.run(
         system=simple_system, line=simple_line, init_result=result
@@ -129,8 +146,10 @@ def test_restart(simple_system, simple_line):
     assert_res_equal(result, result2)
 
 
-# test restart with changed pos_tol
 def test_restart_2(weyl_system, weyl_line):
+    """
+    Test restarting from an init_result with changed pos_tol.
+    """
     result1 = z2pack.line.run(system=weyl_system, line=weyl_line)
     result2 = z2pack.line.run(system=weyl_system, line=weyl_line, pos_tol=0.2)
     result2 = z2pack.line.run(
@@ -140,9 +159,12 @@ def test_restart_2(weyl_system, weyl_line):
 
 
 def test_invalid_restart(simple_system, simple_line):
+    """
+    Test conflicting 'init_result' and 'load' option.
+    """
     result = z2pack.line.run(system=simple_system, line=simple_line)
     with pytest.raises(ValueError):
-        result2 = z2pack.line.run(
+        z2pack.line.run(
             system=simple_system,
             line=simple_line,
             init_result=result,
@@ -151,23 +173,29 @@ def test_invalid_restart(simple_system, simple_line):
 
 
 def test_file_restart(simple_system, simple_line):
-    with tempfile.NamedTemporaryFile() as fp:
-        result = z2pack.line.run(
-            system=simple_system, line=simple_line, save_file=fp.name
+    """
+    Test restart from a savefile.
+    """
+    with tempfile.NamedTemporaryFile() as temp_file:
+        result1 = z2pack.line.run(
+            system=simple_system, line=simple_line, save_file=temp_file.name
         )
         result2 = z2pack.line.run(
             system=simple_system,
             line=simple_line,
-            save_file=fp.name,
+            save_file=temp_file.name,
             load=True,
             serializer=json
         )
-    assert_res_equal(result, result2)
+    assert_res_equal(result1, result2)
 
 
 def test_load_inexisting(simple_system, simple_line):
+    """
+    Test that trying to load from an inexisting file raises.
+    """
     with pytest.raises(IOError):
-        result = z2pack.line.run(
+        z2pack.line.run(
             system=simple_system,
             line=simple_line,
             save_file='invalid_name',
@@ -177,32 +205,26 @@ def test_load_inexisting(simple_system, simple_line):
         )
 
 
-def test_load_inconsistent(simple_system, simple_line):
-    with pytest.raises(ValueError):
-        result = z2pack.line.run(
-            system=simple_system,
-            line=simple_line,
-            init_result='bla',
-            save_file='invalid_name',
-            load=True
-        )
-
-
 def test_load_no_filename(simple_system, simple_line):
+    """
+    Test that trying to load without a filename given raises.
+    """
     with pytest.raises(ValueError):
-        result = z2pack.line.run(
-            system=simple_system, line=simple_line, load=True
-        )
+        z2pack.line.run(system=simple_system, line=simple_line, load=True)
 
 
 def test_invalid_line(simple_system):
+    """
+    Test that using a line that is not connected raises.
+    """
     with pytest.raises(ValueError):
-        result = z2pack.line.run(
-            system=simple_system, line=lambda t: [t / 2, 0, 0]
-        )
+        z2pack.line.run(system=simple_system, line=lambda t: [t / 2, 0, 0])
 
 
 def test_invalid_path(simple_system, simple_line):
+    """
+    Test that trying from an invalid path raises.
+    """
     with pytest.raises(ValueError):
         z2pack.line.run(
             system=simple_system,
--- tests/test_log.py
+++ tests/test_log.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Test logging output."""
+# pylint: disable=unused-import,redefined-outer-name
 
-import sys
-from io import StringIO
+import logging
+from io import StringIO  # pylint: disable=no-name-in-module
 from contextlib import contextmanager
 
 import z2pack
-import logging
 
 from hm_systems import simple_system, simple_surface, simple_line
 
@@ -16,6 +15,9 @@ IGNORE_LINES = [
 
 
 def compare_lines(x, y):
+    """
+    Compare two outputs, skipping the IGNORE_LINES.
+    """
     for xline, yline in zip(x.splitlines(), y.splitlines()):
         if any((part in xline) and (part in yline) for part in IGNORE_LINES):
             continue
@@ -24,10 +26,13 @@ def compare_lines(x, y):
 
 
 @contextmanager
-def CaptureLoggingOutput(compare_data):
+def capture_logging_output(compare_data):
+    """
+    Context manager that captures the logging output, and compares it to the previously obtained value.
+    """
     out = StringIO()
     handler = logging.StreamHandler(stream=out)
-    handler.setFormatter(z2pack._logging_format.DefaultFormatter())
+    handler.setFormatter(z2pack._logging_format.DefaultFormatter())  # pylint: disable=protected-access
     logger = logging.getLogger('z2pack')
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
@@ -40,12 +45,10 @@ def CaptureLoggingOutput(compare_data):
 
 
 def test_surface_report(compare_data, simple_system, simple_surface):
-    with CaptureLoggingOutput(compare_data):
-        result = z2pack.surface.run(
-            system=simple_system, surface=simple_surface
-        )
+    with capture_logging_output(compare_data):
+        z2pack.surface.run(system=simple_system, surface=simple_surface)
 
 
 def test_line_report(compare_data, simple_system, simple_line):
-    with CaptureLoggingOutput(compare_data):
-        result = z2pack.line.run(system=simple_system, line=simple_line)
+    with capture_logging_output(compare_data):
+        z2pack.line.run(system=simple_system, line=simple_line)

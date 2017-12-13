d83b1f37e48ee7c1f5203928c11759cb019061ea
Unknown <tony.metger@gmail.com>
2017-12-13 16:53:13 +0100

    Add test for symmetry projection of fp system

---
+++ tests/test_read_dmn.py
@@ -0,0 +1,15 @@
+import pytest
+import z2pack
+import numpy as np
+
+
+def test_read(compare_data):
+    compare_data(
+        lambda x, y: np.equal(x, y).all(),
+        z2pack.fp._read_dmn.get_dmn('samples/dmn/bi.dmn')
+    )
+
+
+def test_false_path():
+    with pytest.raises(IOError):
+        z2pack.fp._read_dmn.get_dmn('invalid_path')

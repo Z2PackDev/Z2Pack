af3c53d3ccdba4d18bdee9fda0a4686fe1887b0b
euclidmenot2 <62659779+euclidmenot2@users.noreply.github.com>
2021-03-25 21:56:44 -0400

    Add files via upload

--- examples/fp/elk/Bi/run.py
+++ examples/fp/elk/Bi/run.py
@@ -26,8 +26,6 @@ if not os.path.exists('./ground'):
     shutil.copyfile('input/elk.in', 'ground/elk.in')
     subprocess.call(elkdir + ' >& elkWannier.log', shell=True, cwd='./ground')
 
-
-
 # Collecting the files for the surface calculation
 # The k-point nearest neighbors list/kpoints string is appended to the .in file,
 # starting on the last line, and there can be no extra lines in between the shell_list
@@ -49,20 +47,25 @@ input_files = [
 shutil.copyfile('input/elkWannBands.in', 'ground/elk.in')
 
 # Create the Z2Pack system.
-system = z2pack.fp.System(input_files=input_files,
+system = z2pack.fp.System(
+                          input_files=input_files,
                           kpt_fct=z2pack.fp.kpoint.elk,
                           kpt_path="elk.in",
                           command=elkdir + ' >& elk.log',
-                          mmn_path='wannier.mmn')
+                          mmn_path='wannier.mmn'
+						  )
 
 # Run the WCC calculations
-result_0 = z2pack.surface.run(system=system,
+result_0 = z2pack.surface.run(
+                              system=system,
                               surface=lambda s, t: [0, s / 2, t],
                               save_file='./results/res_0.json',
-                              load=True)
+                              load=True
+							  )
 
 print('Z2 topological invariant at kx = 0: {0}'.format(
-    z2pack.invariant.z2(result_0)))
+    z2pack.invariant.z2(result_0))
+	)
 
 # Plot the WCC
 fig, ax = plt.subplots(1, 1, sharey=True, figsize=(9, 5))
